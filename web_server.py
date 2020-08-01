0de0552a13d0345e1ace81e80dcd7a8e70868a00
HyperCodeMarkup <hypercodemarkup@gmail.com>
2020-08-01 07:53:32 +0100

    Messages added to index when loading

--- web_server.py
+++ web_server.py
@@ -1,23 +1,25 @@
 from flask import Flask, render_template
 from flask import redirect, url_for, request
 
-from flask_sqlalchemy import SQLAlchemy
+# from flask_sqlalchemy import SQLAlchemy
 
-from main_server import *
+# from main_server import *
 
 web_server = Flask(__name__)
 
-web_server.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///DataBase.sqlite3'
-web_server.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-DataBase = SQLAlchemy(web_server)
+# web_server.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///DataBase.sqlite3'
+# web_server.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+# DataBase = SQLAlchemy(web_server)
+
+smsg = ["Hello", "New Message", "This is a message"]
 
 """ ROUTING """
 
 @web_server.route("/")
 @web_server.route("/home")
 def index():
-    GetDatabaseData()
-    return render_template("index.html")
+    # GetDatabaseData()
+    return render_template("index.html", messages=smsg)
 
 @web_server.route("/newUser", methods=["POST", "GET"])
 def newUser():
@@ -25,12 +27,12 @@ def newUser():
         usr = request.form["username"]
         pas = request.form["password"]
 
-        found_user = QueryDatabase("Username", usr)
+        # found_user = QueryDatabase("Username", usr)
 
-        if found_user == False:
-            new = DataBase(usr, pas)
-            db.session.add(new)
-            db.session.commit()
+        # if found_user == False:
+        #     new = DataBase(usr, pas)
+        #     db.session.add(new)
+        #     db.session.commit()
         
         return render_template("index.html")
     else:
@@ -44,5 +46,5 @@ def error404(error):
     return render_template("error.html", EC="Error! Page Not Found!"), 404
 
 if __name__ == "__main__":
-    DataBase.create_all()
+    # DataBase.create_all()
     web_server.run(port="8080")
